7d55aa718c1de2b65859534f57c57c19311ddda2
Matt Sadowski <github@yourmatt.com>
2017-04-24 08:30:33 -0600

    Added database definition

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dyspro-backup-manager
+
+## Synopsis
 A command line utility for creating offsite backups to a local network from a server environment.
+
+## Installation
+
+### Database
+
+This requires a MySQL database to manage server connections, backups schedules, and provide a log of backup activity.
+
+1.  Create the database user that this will use.
+2.  Run the docs/create-database.sql script.
---
+++ docs/create-database.sql
@@ -0,0 +1,35 @@
+
+CREATE DATABASE DysproBackupManager;
+USE DysproBackupManager;
+
+CREATE TABLE Servers
+(            ServerId INT AUTO_INCREMENT PRIMARY KEY
+,            ServerName VARCHAR(100) NOT NULL
+,            UserName VARCHAR(100) NOT NULL
+,            SSHKeyFileLocation VARCHAR(250) NOT NULL
+,            PickupDirectoryLocation VARCHAR(250) NOT NULL);
+
+CREATE TABLE Schedules
+(            ScheduleId INT AUTO_INCREMENT PRIMARY KEY
+,            ServerId INT NOT NULL
+,            DropoffDirectoryLocation VARCHAR(250) NOT NULL
+,            CleanupBackups BIT NOT NULL DEFAULT 0
+,            FOREIGN KEY (ServerId) REFERENCES Servers(ServerId));
+
+CREATE TABLE BackupLog
+(            BackupLogId INT AUTO_INCREMENT PRIMARY KEY
+,            ScheduleId INT NOT NULL
+,            DateStart DATETIME NOT NULL
+,            DateFinish DATETIME
+,            FOREIGN KEY (ScheduleId) REFERENCES Schedules(ScheduleId));
+
+CREATE TABLE BackupLogFiles
+(            BackupLogFileId INT AUTO_INCREMENT PRIMARY KEY
+,            BackupLogId INT NOT NULL
+,            FileName VARCHAR(250) NOT NULL
+,            FileSize BIGINT UNSIGNED NOT NULL
+,            DateCreated DATETIME NOT NULL
+,            DateDeleted DATETIME
+,            FOREIGN KEY (BackupLogId) REFERENCES BackupLog(BackupLogId));
+
+SHOW TABLES;
